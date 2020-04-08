c89a668647122204622f12da86a14c2290bb8f2c
OOZYGRUB <OozyGrub@hotmail.com>
2020-04-08 19:42:25 +0700

    Updates

---
+++ precache-manifest.9452641564c1510b969502ea43780d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8334b59f040de704b1d9a771e987ec80",
+    "url": "/korn-k-website/index.html"
+  },
+  {
+    "revision": "666944372a7d8dc595b3",
+    "url": "/korn-k-website/static/css/main.d43e038b.chunk.css"
+  },
+  {
+    "revision": "c408be6e4180e75c166c",
+    "url": "/korn-k-website/static/js/2.c61ab58a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/korn-k-website/static/js/2.c61ab58a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "666944372a7d8dc595b3",
+    "url": "/korn-k-website/static/js/main.5dfd6855.chunk.js"
+  },
+  {
+    "revision": "0537a8130da80973385a",
+    "url": "/korn-k-website/static/js/runtime-main.e5f0361f.js"
+  }
+]);
