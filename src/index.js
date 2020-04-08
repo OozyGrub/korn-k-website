9edc38af331098d1cc01c6afaa2de39d02a1b68f
OOZYGRUB <OozyGrub@hotmail.com>
2020-04-08 19:58:25 +0700

    Edit react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
 import "./index.css";
+
+import { BrowserRouter as Router } from "react-router-dom";
  
 ReactDOM.render(
-  <Main/>, 
+  <Router>
+    <Main/>
+  </Router>,
   document.getElementById("root")
 );
